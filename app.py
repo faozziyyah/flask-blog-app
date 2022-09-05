e2ae3d2af49c723ea1fd22cc21199d678f592c7c
Your name <omowunmidaud1@gmail.com>
2022-09-05 10:15:57 +0100

    installed flask-wtf

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask, render_template
+#from curses import flash
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+#create form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
+    flash("Welcome to our website!")
     return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name=name)
+
+# invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+#create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("form successfully submitted!")
+
+    return render_template("name.html", 
+        name = name, 
+        form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
