1bfdefeb65cb518094569ab6d78c49760bf7dc66
Your name <omowunmidaud1@gmail.com>
2022-09-07 07:17:38 +0100

    added post model

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #from curses import flash
+from turtle import title
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
+from wtforms.widgets import TextArea
 
 app = Flask(__name__)
 
@@ -14,22 +17,77 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:OPEyemi2001@localh
 app.config['SECRET_KEY'] = "secret"
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#create a blog post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("content", validators=[DataRequired()], widget=TextArea())
+    author = StringField("author", validators=[DataRequired()])
+    slug = StringField("slug", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# add post page
+@app.route("/add-post", methods=["GET", "POST"])
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=form.author.data, slug=form.slug.data)
+        form.title.data = ''
+        form.content.data = ''
+        form.author.data = ''
+        form.slug.data = ''
+
+        db.session.add(post)
+        db.session.commit()
+
+        flash("post submitted successfully!")
+
+    return render_template("add_post.html", form=form)
 
 #create model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Name %r>' % self.name
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("user deleted successfully!!")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html", form=form, name=name, our_users=our_users)
+
+    except:
+        flash("Whoops! Something went wrong, please try again later...")
+        return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 #create form class
 
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
     submit = SubmitField("Submit")
 class NamerForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
@@ -43,6 +101,7 @@ def update(id):
     if request.method == 'POST':
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
         try:
             db.session.commit()
             flash("user updated successfully!")
@@ -51,7 +110,7 @@ def update(id):
             flash("Error! try again later")
             return render_template("update.html", form=form, name_to_update = name_to_update)
     else:
-        return render_template("update.html", form=form, name_to_update = name_to_update)
+        return render_template("update.html", form=form, name_to_update = name_to_update, id = id)
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
@@ -60,12 +119,13 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash("User Added successfully!")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html", form=form, name=name, our_users=our_users)
