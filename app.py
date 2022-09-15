90beb1a89f1b2fef5553802583b8bfe633d1116e
Your name <omowunmidaud1@gmail.com>
2022-09-15 14:27:21 +0100

    tweaked app for heroku

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ app = Flask(__name__)
 ckeditor = CKEditor(app)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:OPEyemi2001@localhost/our_users'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:OPEyemi2001@localhost/our_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wekzlufdlvaqdt:38a6719d02dbbe70f04d3e5aeb23b35a279621e9036800d3ece490a962bcb017@ec2-18-209-78-11.compute-1.amazonaws.com:5432/dcrbvlgqnssh2i'
 
 app.config['SECRET_KEY'] = "secret"
 
